eb54cfeea9baedc32c1f656c827e849d781ba230
PavanSaiNekkala <91945047+PavanSaiNekkala@users.noreply.github.com>
2026-05-11 16:46:57 +0530

    Update nse_collector.py

--- collectors/nse_collector.py
+++ collectors/nse_collector.py
@@ -1,45 +1,64 @@
 import pandas as pd
 from nsepython import nse_eq_symbols
-from datetime import datetime
 import os
+from datetime import datetime
 
 
-def fetch_nse_stock_universe():
+OUTPUT_FILE = "data/raw/nse_stock_universe.csv"
+
+
+def fetch_nse_universe():
 
     print("Fetching NSE stock universe...")
 
     symbols = nse_eq_symbols()
 
-    df = pd.DataFrame({
-        "SYMBOL": symbols
-    })
+    cleaned_symbols = []
 
-    df["FETCH_DATE"] = datetime.now().strftime("%Y-%m-%d")
+    for symbol in symbols:
+
+        cleaned_symbols.append(
+            f"{symbol}.NS"
+        )
+
+    df = pd.DataFrame({
+
+        "SYMBOL": cleaned_symbols,
+
+        "FETCH_DATE": datetime.now().strftime(
+            "%Y-%m-%d"
+        )
+    })
 
     return df
 
 
-def save_stock_universe(df):
+def save_universe(df):
 
-    os.makedirs("data/raw", exist_ok=True)
+    os.makedirs(
+        "data/raw",
+        exist_ok=True
+    )
 
-    output_path = "data/raw/nse_stock_universe.csv"
+    df.to_csv(
+        OUTPUT_FILE,
+        index=False
+    )
 
-    df.to_csv(output_path, index=False)
-
-    print(f"Saved stock universe to: {output_path}")
+    print(f"\nSaved to: {OUTPUT_FILE}")
 
 
 def main():
 
-    df = fetch_nse_stock_universe()
+    df = fetch_nse_universe()
 
     print(df.head())
 
     print(f"\nTotal Stocks: {len(df)}")
 
-    save_stock_universe(df)
+    save_universe(df)
 
 
 if __name__ == "__main__":
+
     main()
